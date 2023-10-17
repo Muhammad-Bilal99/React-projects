df7976f6cf2482a2524563121e8def7a011bd98c
Muhammad Bilal <beastop975@gmail.com>
2023-10-17 16:58:52 +0500

    Expense Tracker app, created components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,11 @@
-import PropTypes from 'prop-types'
-import { useLocation } from 'react-router-dom'
-import Button from './Button'
+import React from 'react'
 
-const Header = ({ title, onAdd, showAdd }) => {
-  const location = useLocation()
-
+export const Header = () => {
   return (
-    <header className='header'>
-      <h1>{title}</h1>
-      {location.pathname === '/' && (
-        <Button
-          color={showAdd ? 'red' : 'green'}
-          text={showAdd ? 'Close' : 'Add'}
-          onClick={onAdd}
-        />
-      )}
-    </header>
+    <h2>
+      Expense Tracker
+    </h2>
   )
 }
 
-Header.defaultProps = {
-  title: 'Task Tracker',
-}
 
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-}
-
-// CSS in JS
-// const headingStyle = {
-//   color: 'red',
-//   backgroundColor: 'black',
-// }
-
-export default Header
